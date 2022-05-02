72892ec37eb45a5fe3d616f083877d745f5ce6f4
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2022-05-02 18:19:42 -0300

    Add files via upload

---
+++ contracts/FabioTokenSale.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.5.0;
+
+import "./FabioToken.sol";
+
+contract FabioTokenSale {
+    address payable admin;
+    GetoToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(GetoToken _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    /*
+    Permite que uma conta (endereço chamador do método) compre uma determinada quantidade de tokens
+    */
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(
+            msg.value == _numberOfTokens * tokenPrice,
+            "Número de tokens não corresponde ao valor"
+        );
+        require(
+            tokenContract.balanceOf(address(this)) >= _numberOfTokens,
+            "Contrato não tem tokens suficientes"
+        );
+        require(
+            tokenContract.transfer(msg.sender, _numberOfTokens),
+            "Algum problema com a transferência de tokens"
+        );
+        tokensSold += _numberOfTokens;
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+
+    function _forwardFunds() internal {
+     admin.transfer(msg.value);
+    }
+
+    /*
+    Permite que uma conta (somente o admnistrador) finalize a venda de tokens e transfira tokens para a conta do admim
+    */
+
+    function endSale() public {
+        require(msg.sender == admin, "Apenas o administrador pode chamar essa função");
+        require(
+            tokenContract.transfer(
+                msg.sender,
+                tokenContract.balanceOf(address(this))
+            ),
+            "Incapaz de transferir tokens para administrador"
+        );
+        // destroy contract
+        selfdestruct(admin);
+    }
+}
